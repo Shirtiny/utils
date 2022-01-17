f3820df6b55f90261636fd6ceb1c17932bd895d2
shirtiny <shirtiny@gmail.com>
2022-01-17 11:53:56 +0800

    v0.1.0

--- src/__tests__/base64.test.ts
+++ src/__tests__/base64.test.ts
@@ -5,7 +5,7 @@
 /*
  * @Author: Shirtiny
  * @Date: 2022-01-14 16:22:41
- * @LastEditTime: 2022-01-16 14:34:21
+ * @LastEditTime: 2022-01-17 11:52:23
  * @Description:
  */
 import base64 from "../lib/base64";
@@ -17,29 +17,96 @@ if (typeof TextEncoder === "undefined") {
 }
 
 describe("base64", () => {
+  // const view = new DataView(buffer, 0);
+  // view.setInt8(0, 65);
+  // view.setInt8(1, 66);
+  // view.setInt8(2, 67);
+  const ABC = "ABC";
+  const ABC_BASE64 = "QUJD";
+  const abc = new Uint8Array([65, 66, 67]);
+
+  const HA_HA = "哈哈";
+  const HA_HA_BASE64 = "5ZOI5ZOI";
+  const haHa = new Uint8Array([229, 147, 136, 229, 147, 136]);
+
+  const SUFFIX = "setInt8";
+  const SUFFIX_BASE64 = "c2V0SW50OA==";
+  const NO_SUFFIX_BASE64 = "c2V0SW50OA";
+
+  const SH_URL = "http://shirtiny.cn/1?a=23&b=34";
+  const SH_URL_BASE64 = "aHR0cDovL3NoaXJ0aW55LmNuLzE/YT0yMyZiPTM0";
+  const SH_URL_BASE64_URL_SAFE = "aHR0cDovL3NoaXJ0aW55LmNuLzE_YT0yMyZiPTM0";
+  const shUrl = new Uint8Array([
+    104, 116, 116, 112, 58, 47, 47, 115, 104, 105, 114, 116, 105, 110, 121, 46,
+    99, 110, 47, 49, 63, 97, 61, 50, 51, 38, 98, 61, 51, 52,
+  ]);
+
+  const LONG_URL =
+    "https://cn.bing.com/search???????????????????q=base64+url_safe+%e5%9c%a8%e7%ba%bf&qs=n&sp=-1&pq=base64+url_safe+&sc=0-16&sk=&cvid=B26F25E3D80A4AC2B58643AF9F5F6040&first=16&FORM=PERE1";
+  const LONG_URL_BASE64 =
+    "aHR0cHM6Ly9jbi5iaW5nLmNvbS9zZWFyY2g/Pz8/Pz8/Pz8/Pz8/Pz8/Pz8/cT1iYXNlNjQrdXJsX3NhZmUrJWU1JTljJWE4JWU3JWJhJWJmJnFzPW4mc3A9LTEmcHE9YmFzZTY0K3VybF9zYWZlKyZzYz0wLTE2JnNrPSZjdmlkPUIyNkYyNUUzRDgwQTRBQzJCNTg2NDNBRjlGNUY2MDQwJmZpcnN0PTE2JkZPUk09UEVSRTE=";
+  const LONG_URL_BASE64_URL_SAFE =
+    "aHR0cHM6Ly9jbi5iaW5nLmNvbS9zZWFyY2g_Pz8_Pz8_Pz8_Pz8_Pz8_Pz8_cT1iYXNlNjQrdXJsX3NhZmUrJWU1JTljJWE4JWU3JWJhJWJmJnFzPW4mc3A9LTEmcHE9YmFzZTY0K3VybF9zYWZlKyZzYz0wLTE2JnNrPSZjdmlkPUIyNkYyNUUzRDgwQTRBQzJCNTg2NDNBRjlGNUY2MDQwJmZpcnN0PTE2JkZPUk09UEVSRTE=";
+
   it("ab2str", () => {
-    expect(base64.ab2str([65, 66, 67])).toBe("ABC");
-    expect(base64.ab2str([229, 147, 136, 229, 147, 136])).toBe("哈哈");
+    expect(base64.ab2str(abc)).toBe(ABC);
+    expect(base64.ab2str(haHa)).toBe(HA_HA);
+    expect(base64.ab2str(shUrl)).toBe(SH_URL);
   });
 
   it("str2ab", () => {
-    // const view = new DataView(buffer, 0);
-    // view.setInt8(0, 65);
-    // view.setInt8(1, 66);
-    // view.setInt8(2, 67);
-    const arr = new Uint8Array(new ArrayBuffer(3));
-    arr[0] = 65;
-    arr[1] = 66;
-    arr[2] = 67;
-    expect(base64.str2ab("ABC")).toEqual(arr);
-    const arr2 = new Uint8Array(new ArrayBuffer(6));
-    arr2[0] = 229;
-    arr2[1] = 147;
-    arr2[2] = 136;
-    arr2[3] = 229;
-    arr2[4] = 147;
-    arr2[5] = 136;
-    expect(base64.str2ab("哈哈")).toEqual(arr2);
+    expect(base64.str2ab(ABC)).toEqual(abc);
+    expect(base64.str2ab(HA_HA)).toEqual(haHa);
+    expect(base64.str2ab(SH_URL)).toEqual(shUrl);
+  });
+
+  it("ab2Base64", () => {
+    expect(base64.ab2Base64(abc)).toBe(ABC_BASE64);
+    expect(base64.ab2Base64(haHa)).toBe(HA_HA_BASE64);
+    expect(base64.ab2Base64(shUrl)).toBe(SH_URL_BASE64);
+  });
+
+  it("base642ab", () => {
+    expect(base64.base642ab(ABC_BASE64)).toEqual(abc);
+    expect(base64.base642ab(HA_HA_BASE64)).toEqual(haHa);
+    expect(base64.base642ab(SH_URL_BASE64)).toEqual(shUrl);
+  });
+
+  it("toBase64", () => {
+    expect(base64.toBase64(ABC)).toBe(ABC_BASE64);
+    expect(base64.toBase64(HA_HA)).toBe(HA_HA_BASE64);
+    expect(base64.toBase64(SH_URL)).toBe(SH_URL_BASE64);
+    expect(base64.toBase64(SUFFIX)).toBe(SUFFIX_BASE64);
+    expect(base64.toBase64(LONG_URL)).toBe(LONG_URL_BASE64);
+  });
+
+  it("fromBase64", () => {
+    expect(base64.fromBase64(ABC_BASE64)).toBe(ABC);
+    expect(base64.fromBase64(HA_HA_BASE64)).toBe(HA_HA);
+    expect(base64.fromBase64(SH_URL_BASE64)).toBe(SH_URL);
+    expect(base64.fromBase64(LONG_URL_BASE64)).toBe(LONG_URL);
+    expect(base64.fromBase64(SUFFIX_BASE64)).toBe(SUFFIX);
+    expect(base64.fromBase64(NO_SUFFIX_BASE64)).toBe(SUFFIX);
+  });
+
+  it("toUrlSafe", () => {
+    expect(base64.toUrlSafe(SH_URL_BASE64)).toBe(SH_URL_BASE64_URL_SAFE);
+    expect(base64.toUrlSafe(LONG_URL_BASE64)).toBe(LONG_URL_BASE64_URL_SAFE);
+  });
+
+  it("fromUrlSafe", () => {
+    expect(base64.fromUrlSafe(SH_URL_BASE64_URL_SAFE)).toBe(SH_URL_BASE64);
+    expect(base64.fromUrlSafe(LONG_URL_BASE64_URL_SAFE)).toBe(LONG_URL_BASE64);
+  });
+
+  it("toBase64Url", () => {
+    expect(base64.toBase64Url(SH_URL)).toBe(SH_URL_BASE64_URL_SAFE);
+    expect(base64.toBase64Url(LONG_URL)).toBe(LONG_URL_BASE64_URL_SAFE);
+  });
+
+  it("toBase64Url", () => {
+    expect(base64.fromBase64url(SH_URL_BASE64_URL_SAFE)).toBe(SH_URL);
+    expect(base64.fromBase64url(LONG_URL_BASE64_URL_SAFE)).toBe(LONG_URL);
   });
 
   it("padSuffix", () => {
@@ -47,12 +114,10 @@ describe("base64", () => {
     expect(base64.padSuffix("Y0dGa0c")).toBe("Y0dGa0c=");
     expect(base64.padSuffix("c2V0SW50OA")).toBe("c2V0SW50OA==");
   });
-
-  it("ab2Base64", () => {
-    expect(base64.ab2Base64(base64.str2ab("ABC"))).toBe("QUJD");
-    expect(base64.ab2Base64(base64.str2ab("中文"))).toBe("5Lit5paH");
-    expect(
-      base64.ab2Base64(base64.str2ab("http://shirtiny.cn/1?a=23&b=34")),
-    ).toBe("aHR0cDovL3NoaXJ0aW55LmNuLzE/YT0yMyZiPTM0");
+  
+  it("removeSuffix", () => {
+    expect(base64.removeSuffix("cGFkU3VmZml4")).toBe("cGFkU3VmZml4");
+    expect(base64.removeSuffix("Y0dGa0c=")).toBe("Y0dGa0c");
+    expect(base64.removeSuffix("c2V0SW50OA==")).toBe("c2V0SW50OA");
   });
 });
--- src/lib/base64.ts
+++ src/lib/base64.ts
@@ -1,14 +1,14 @@
 /*
  * @Author: Shirtiny
  * @Date: 2022-01-14 16:01:01
- * @LastEditTime: 2022-01-16 14:33:47
+ * @LastEditTime: 2022-01-17 11:52:10
  * @Description:
  */
 
 import logger from "../utils/logger";
 
 /**
- *  字节数组转字符串
+ * @description 字节数组转字符串
  * @param {Iterable<number>} buffer
  * @param {boolean} noEncode 不使用utf8解码
  * @returns {string}
@@ -24,28 +24,108 @@ const ab2str = (
 };
 
 /**
- * 字符串转字节数组
+ * @description 字符串转字节数组
  * @param {string} str
+ * @param {boolean} noEncode 不使用utf8解码
  * @returns {Uint8Array} Uint8Array
  */
-const str2ab = (str: string): Uint8Array => {
-  // const buffer = new ArrayBuffer(str.length);
-  // const bufferView = new Uint8Array(buffer);
-  // for (let i = 0, len = str.length; i < len; i++) {
-  //   bufferView[i] = str.charCodeAt(i);
-  // }
+const str2ab = (str: string, noEncode: boolean = false): Uint8Array => {
+  if (noEncode) {
+    const buffer = new ArrayBuffer(str.length);
+    const bufferView = new Uint8Array(buffer);
+    for (let i = 0, len = str.length; i < len; i++) {
+      bufferView[i] = str.charCodeAt(i);
+    }
+    return bufferView;
+  }
 
   return new Uint8Array(new TextEncoder().encode(str).buffer);
 };
 
 /**
- * 扩展base64后缀
- * @param input base64字符串
- * @returns
+ * @description 字节数组转Base64
+ * @param {Iterable<number>} buffer
+ * @returns {string}
  */
-const padSuffix = (input: string): string => {
+const ab2Base64 = (buffer: Iterable<number>): string => {
+  const str = ab2str(buffer, true);
+  return window.btoa(str);
+};
+
+/**
+ * @description base64字符串转字节数组
+ * @param {string} base64Input base64字符串
+ * @returns {Uint8Array} Uint8Array
+ */
+const base642ab = (base64Input: string): Uint8Array => {
+  const str = window.atob(base64Input);
+  return str2ab(str, true);
+};
+
+/**
+ * @description 转为base64字符串
+ * @param {string} str 字符串
+ * @returns {string} base64字符串
+ */
+const toBase64 = (str: string): string => {
+  const ab = str2ab(str);
+  return ab2Base64(ab);
+};
+
+/**
+ * @description 从base64字符串解析
+ * @param {string} base64Input base64字符串
+ * @returns {string} 字符串
+ */
+const fromBase64 = (base64Input: string): string => {
+  const ab = base642ab(base64Input);
+  return ab2str(ab);
+};
+
+/**
+ * @description 将base64转为ur传输安全的格式 base64 url_safe
+ * @param {string} base64Input base64字符串
+ * @returns {string}
+ */
+const toUrlSafe = (base64Input: string): string => {
+  const str = base64Input.replace(/\+/g, "-").replace(/\//g, "_");
+  return str;
+};
+
+/**
+ * @description 从base64 url_safe 转为base64
+ * @param {string} base64Input base64字符串
+ * @returns {string}
+ */
+const fromUrlSafe = (base64Input: string): string => {
+  const str = base64Input.replace(/-/g, "+").replace(/_/g, "/");
+  return str;
+};
+
+/**
+ * @description 将字符串转为base64 url_safe
+ * @param {string} base64Input base64字符串
+ * @returns {string}
+ */
+const toBase64Url = (str: string): string => toUrlSafe(toBase64(str));
+
+/**
+ * @description 从base64 url_safe解析
+ * @param {string} base64UrlInput base64 url_safe字符串
+ * @returns {string}
+ */
+const fromBase64url = (base64UrlInput: string): string => {
+  return fromBase64(fromUrlSafe(base64UrlInput));
+};
+
+/**
+ * @description 填充base64后的等号
+ * @param {string} base64Input base64字符串
+ * @returns {string}
+ */
+const padSuffix = (base64Input: string): string => {
   let num = 0;
-  const mo = input.length % 4;
+  const mo = base64Input.length % 4;
   switch (mo) {
     case 3:
       num = 1;
@@ -57,21 +137,42 @@ const padSuffix = (input: string): string => {
       num = 0;
       break;
     default:
-      logger.error("base64", "padSuffix", "无效的base64Url长度", input, mo);
+      logger.error(
+        "base64",
+        "padSuffix",
+        "无效的base64Url长度",
+        base64Input,
+        mo,
+      );
       return "";
   }
 
-  return input + "=".repeat(num);
+  return base64Input + "=".repeat(num);
 };
 
 /**
- *  字节数组转Base64
- * @param {Iterable<number>} buffer
+ * @description 移除base64后的等号
+ * @param {string} base64Input base64字符串
  * @returns {string}
  */
-const ab2Base64 = (buffer: Iterable<number>): string =>
-  window.btoa(ab2str(buffer, true));
+const removeSuffix = (base64Input: string): string => {
+  const pos = base64Input.indexOf("=");
+  return pos > 0 ? base64Input.slice(0, pos) : base64Input;
+};
 
-const base64 = { ab2str, str2ab, padSuffix, ab2Base64 };
+const base64 = {
+  ab2str,
+  str2ab,
+  ab2Base64,
+  base642ab,
+  toBase64,
+  fromBase64,
+  toUrlSafe,
+  fromUrlSafe,
+  toBase64Url,
+  fromBase64url,
+  padSuffix,
+  removeSuffix,
+};
 
 export default base64;
