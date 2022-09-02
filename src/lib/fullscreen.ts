e4514410b5d2dedeec6e26435409b9e56a173c42
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2022-09-02 09:48:59 +0000

    fullscreen remove direct document

--- src/lib/fullscreen.ts
+++ src/lib/fullscreen.ts
@@ -23,57 +23,67 @@ type ONFSCPropName =
   | "onmozfullscreenchange"
   | "MSFullscreenChange";
 
-/**
- * caniuse
- * https://caniuse.com/#search=Fullscreen
- * 参考 MDN, 并不确定是否有o前缀的, 暂时不加入
- * https://developer.mozilla.org/zh-CN/docs/Web/API/Element/requestFullscreen
- * 各个浏览器
- * https://www.wikimoe.com/?post=82
- */
-const DOC_EL = document.documentElement;
+function prepareEnv() {
+  /**
+   * caniuse
+   * https://caniuse.com/#search=Fullscreen
+   * 参考 MDN, 并不确定是否有o前缀的, 暂时不加入
+   * https://developer.mozilla.org/zh-CN/docs/Web/API/Element/requestFullscreen
+   * 各个浏览器
+   * https://www.wikimoe.com/?post=82
+   */
+  const DOC_EL = document.documentElement;
 
-let RFC_METHOD_NAME: RFSMethodName = "requestFullscreen";
-let EFS_METHOD_NAME: EFSMethodName = "exitFullscreen";
-let FSE_PROP_NAME: FSEPropName = "fullscreenElement";
-let ON_FSC_PROP_NAME: ONFSCPropName = "onfullscreenchange";
+  let RFC_METHOD_NAME: RFSMethodName = "requestFullscreen";
+  let EFS_METHOD_NAME: EFSMethodName = "exitFullscreen";
+  let FSE_PROP_NAME: FSEPropName = "fullscreenElement";
+  let ON_FSC_PROP_NAME: ONFSCPropName = "onfullscreenchange";
 
-if (`webkitRequestFullScreen` in DOC_EL) {
-  RFC_METHOD_NAME = "webkitRequestFullScreen";
-  EFS_METHOD_NAME = "webkitExitFullscreen";
-  FSE_PROP_NAME = "webkitFullscreenElement";
-  ON_FSC_PROP_NAME = "onwebkitfullscreenchange";
-} else if (`msRequestFullscreen` in DOC_EL) {
-  RFC_METHOD_NAME = "msRequestFullscreen";
-  EFS_METHOD_NAME = "msExitFullscreen";
-  FSE_PROP_NAME = "msFullscreenElement";
-  ON_FSC_PROP_NAME = "MSFullscreenChange";
-} else if (`mozRequestFullScreen` in DOC_EL) {
-  RFC_METHOD_NAME = "mozRequestFullScreen";
-  EFS_METHOD_NAME = "mozCancelFullScreen";
-  FSE_PROP_NAME = "mozFullScreenElement";
-  ON_FSC_PROP_NAME = "onmozfullscreenchange";
-} else if (!(`requestFullscreen` in DOC_EL)) {
-  logger.warn("fullscreen", "selfExec", `当前浏览器不支持Fullscreen API !`);
+  if (`webkitRequestFullScreen` in DOC_EL) {
+    RFC_METHOD_NAME = "webkitRequestFullScreen";
+    EFS_METHOD_NAME = "webkitExitFullscreen";
+    FSE_PROP_NAME = "webkitFullscreenElement";
+    ON_FSC_PROP_NAME = "onwebkitfullscreenchange";
+  } else if (`msRequestFullscreen` in DOC_EL) {
+    RFC_METHOD_NAME = "msRequestFullscreen";
+    EFS_METHOD_NAME = "msExitFullscreen";
+    FSE_PROP_NAME = "msFullscreenElement";
+    ON_FSC_PROP_NAME = "MSFullscreenChange";
+  } else if (`mozRequestFullScreen` in DOC_EL) {
+    RFC_METHOD_NAME = "mozRequestFullScreen";
+    EFS_METHOD_NAME = "mozCancelFullScreen";
+    FSE_PROP_NAME = "mozFullScreenElement";
+    ON_FSC_PROP_NAME = "onmozfullscreenchange";
+  } else if (!(`requestFullscreen` in DOC_EL)) {
+    logger.warn("fullscreen", "selfExec", `当前浏览器不支持Fullscreen API !`);
+  }
+
+  return {
+    DOC_EL,
+    RFC_METHOD_NAME,
+    EFS_METHOD_NAME,
+    FSE_PROP_NAME,
+    ON_FSC_PROP_NAME,
+    document,
+  };
 }
-
 /**
  * 启用全屏
  * @param  {HTMLElement} 元素
  * @param  {FullscreenOptions} 选项
  * @returns {Promise}
  */
-function beFull(
-  el: HTMLElement = DOC_EL,
-  options?: FullscreenOptions,
-): Promise<void> {
-  return el[RFC_METHOD_NAME](options);
+function beFull(el?: HTMLElement, options?: FullscreenOptions): Promise<void> {
+  const { DOC_EL, RFC_METHOD_NAME } = prepareEnv();
+  const element = el ? el : DOC_EL;
+  return element[RFC_METHOD_NAME](options);
 }
 
 /**
  * 退出全屏
  */
 function exitFull(): Promise<void> {
+  const { EFS_METHOD_NAME, document } = prepareEnv();
   return document[EFS_METHOD_NAME]();
 }
 
@@ -81,8 +91,10 @@ function exitFull(): Promise<void> {
  * 元素是否全屏
  * @param {HTMLElement}
  */
-function isFull(el: HTMLElement | EventTarget): boolean {
-  return el === document[FSE_PROP_NAME];
+function isFull(el?: HTMLElement | EventTarget): boolean {
+  const { FSE_PROP_NAME, DOC_EL, document } = prepareEnv();
+  const element = el ? el : DOC_EL;
+  return element === document[FSE_PROP_NAME];
 }
 
 /**
@@ -90,8 +102,8 @@ function isFull(el: HTMLElement | EventTarget): boolean {
  * @param  {HTMLElement} 目标元素
  * @returns {Promise}
  */
- export function toggle(
-  el: HTMLElement = DOC_EL,
+export function toggle(
+  el?: HTMLElement,
   options?: FullscreenOptions,
 ): Promise<void> {
   if (isFull(el)) {
@@ -106,7 +118,7 @@ function isFull(el: HTMLElement | EventTarget): boolean {
  * @param  {HTMLElement} 元素
  * @param  {(isFull: boolean) => void} 返回"是否全屏"
  */
- export function watch(el: HTMLElement, callback: (isFull: boolean) => void) {
+export function watch(el: HTMLElement, callback: (isFull: boolean) => void) {
   const cancel = () => {
     el.onfullscreenchange = null;
   };
@@ -116,6 +128,8 @@ function isFull(el: HTMLElement | EventTarget): boolean {
       callback(isFull(event.target));
     }
   };
+
+  const { ON_FSC_PROP_NAME } = prepareEnv();
 
   // 这里addEventListener不好使
   el[ON_FSC_PROP_NAME] = handler;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 /*
  * @Author: Shirtiny
- * @Date: 2021-09-29 18:17:03
- * @LastEditTime: 2021-11-21 18:23:44
+ * @Date: 2021-09-30 09:20:42
+ * @LastEditTime: 2022-01-14 16:23:15
  * @Description:
  */
-import * as shUtils from "./lib";
-
-export * from "./lib";
-export default shUtils;
+export { default as date } from "./lib/date";
+export { default as dev } from "./lib/dev";
+export { default as file } from "./lib/file";
+export { default as media } from "./lib/media";
+export { default as http } from "./lib/http";
+export { default as lang } from "./lib/lang";
+export { default as math } from "./lib/math";
+export { default as base64 } from "./lib/base64";
+export { default as reactiveX } from "./lib/reactiveX";
+export { default as storage } from "./lib/storage";
+export { default as dom } from "./lib/dom";
+export { default as style } from "./lib/style";
+export { default as jsx } from "./lib/jsx";
+export { default as fullscreen } from "./lib/fullscreen";
+export { default as util } from "./lib/util";
+export * from "./lib/customError";
+export * from "./lib/events";
+export * from "./lib/jsx";
