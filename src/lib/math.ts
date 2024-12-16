9d4ac5a426c4cd22e65720c77a5b6da25cfe6e87
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2024-12-16 02:22:42 +0000

    str2num

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -9,8 +9,18 @@ export const restrict = (value: number, min: number, max: number) => {
   return Math.min(Math.max(value, min), max);
 };
 
+export const str2Num = (input: string) => {
+  if (!input) return 0;
+
+  // 只保留数字、小数点和空白字符
+  const str = String(input).replace(/[^0-9.\s]/g, "");
+  const value = parseFloat(str);
+  return isNaN(value) ? NaN : value;
+}
+
 const math = {
   restrict,
+  str2Num,
 };
 
 export default math;
